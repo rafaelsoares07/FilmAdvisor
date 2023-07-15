fff185a0b689cfd3b532bbe68a78eec34cd837aa
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-07-15 12:20:49 -0300

    feat: :sparkles:

---
+++ templates/footer.php
@@ -0,0 +1,17 @@
+<footer id="footer" class="footer">
+    <div class="social-container">
+        <ul>
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+        </ul>
+    </div>
+    <p>&copy; 2023 - Rafael Soares Gabriel. Todos os direitos reservados.</p>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.js" integrity="sha512-jxHTIcyuk3K/8tFI4UWP/2XfMAnBUL/GCcY0ah39DQfnsG8+YgtAJsOE8fznN+jWqwgLazJMEpmyE9HW1Mmc+A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="<?=$BASE_URL?>js/deleteModal.js"></script>
+</body>
+
+</html>
